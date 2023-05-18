0e226d55919ff45a4f1698e78775e7b750057ed4
Karmen Too <karmen.tanaka@gmail.com>
2023-05-18 15:25:15 -0400

    done

--- queue.js
+++ queue.js
@@ -20,13 +20,34 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
+    if (this.first) {
+      let newNode = new Node(val)
+      this.last.next = newNode
+      this.last = newNode
+      this.size++
 
+    } else {
+      this.first = new Node(val)
+      this.last = this.first
+      this.size++
+    }
+    return undefined
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
+    if (this.size == 0) {
+      throw new Error
+    } else {
+
+      const toReturn = this.first.val
+      this.first = this.first.next
+      this.size--
+      console.log('size is now', this.size)
+      return toReturn
+    }
 
   }
 
@@ -34,12 +55,13 @@ class Queue {
 
   peek() {
 
+    return this.first ? this.first.val : undefined
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    return !this.first ? true : false
   }
 }
 
--- stack.js
+++ stack.js
@@ -20,26 +20,45 @@ class Stack {
   /** push(val): add new value to end of the stack. Returns undefined. */
 
   push(val) {
+    if (this.first) {
+      let newNode = new Node(val)
+      newNode.next = this.first
+      this.first = newNode
 
+    } else {
+      //this is empty
+      this.first = new Node(val)
+      this.last = this.first
+    }
+    this.size++
+    return undefined
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
+    if (this.first) {
+      const toReturn = this.first.val
+      this.first = this.first.next
+      this.size--
+      return toReturn
+    } else {
+      throw new Error
+    }
 
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
-
+    return this.first ? this.first.val : undefined
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return !this.first ? true : false
   }
 }
 
